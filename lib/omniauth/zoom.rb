9b550fa89eb5f377b8ac296fcc849e6e0a11772b
pacafs <paca.fs@gmail.com>
2020-03-28 01:18:24 +0000

    first commit

---
+++ lib/omniauth/zoom.rb
@@ -0,0 +1,8 @@
+require "omniauth/zoom/version"
+
+module Omniauth
+  module Zoom
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/zoom/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Zoom
+    VERSION = "0.1.0"
+  end
+end
