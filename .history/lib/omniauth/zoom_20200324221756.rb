98537b7f3446c3bbd0d1608fb797f9fcf1b785fe
pacafs <paca.fs@gmail.com>
2020-04-01 15:52:30 +0100

    removed history

--- .history/lib/omniauth/zoom_20200324221756.rb
+++
@@ -1,8 +0,0 @@
-require "omniauth/zoom/version"
-
-module Omniauth
-  module Zoom
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- .history/lib/omniauth/zoom_20200324222355.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'omniauth/zoom/version'
-
-module Omniauth
-  module Zoom
-    # Your code goes here...
-  end
-end
